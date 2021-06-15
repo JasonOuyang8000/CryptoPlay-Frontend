98d08105ca6f42df1ad893b61d8fb6f2ef0035e8
Jason Ouyang <jasonouyang8000@gmail.com>
2021-06-14 22:20:00 -0700

    framer motion

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from "react";
+import { motion } from "framer-motion";
+
 
 export default function App() {
 
+  return (
+   <div className="">
 
-  
-
-    return (
-        <div>
-
-        <h1>Hello World TEST</h1>
-        
-
-        </div>
-    )
+       
+   </div>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
+import 'bootstrap/scss/bootstrap.scss';
 import "./style.scss";
+import './reset.scss';
 import App from './App.js';
 import React from "react";
 import ReactDOM from "react-dom";
+
 
 
  
