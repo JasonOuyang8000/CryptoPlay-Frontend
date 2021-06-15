e5b3c071e3ca356ee4d2e9de2b8ec931fb64a0b2
Jason Ouyang <jasonouyang8000@gmail.com>
2021-06-14 22:29:25 -0700

    react fontawesome

--- src/App.js
+++ src/App.js
@@ -6,8 +6,14 @@ export default function App() {
 
   return (
    <div className="">
+  <motion.div
+    animate={{ rotate: 360 }}
+    transition={{ duration: 2 }}
+  >
+      Hello World
 
-       
+  </motion.div>
+
    </div>
   );
 }
