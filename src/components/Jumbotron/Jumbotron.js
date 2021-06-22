724d03551589a7ec99bffb921fa17c8b658813db
Jason Ouyang <jasonouyang8000@gmail.com>
2021-06-21 23:23:22 -0700

    mobile responsive on jumbotron

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -10,14 +10,14 @@ export default function Jumbotron() {
 
      
                 <div className="row h-100">
-                    <div className="col-8 jumbotron-left d-flex align-items-center">
+                    <div className="col-12 col-md-8 jumbotron-left d-flex ">
                        
                         <h1 id="intro-text">
                             Always Shoot For the Moon.
                         </h1>
                    
                     </div>
-                    <div className="col-4 jumbotron-right">
+                    <div className="col-12 col-md-4 jumbotron-right">
                         <img src={Rocket} alt="Rocket" />
                     </div>
                 </div>
