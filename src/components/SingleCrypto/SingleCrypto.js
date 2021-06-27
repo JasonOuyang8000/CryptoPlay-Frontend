d1de2f6d1cdf771d050cd1cbb4d799da7d6801b0
Jason Ouyang <jasonouyang8000@gmail.com>
2021-06-26 17:14:37 -0700

    color scheme per page

--- src/components/SingleCrypto/SingleCrypto.js
+++ src/components/SingleCrypto/SingleCrypto.js
@@ -2,31 +2,35 @@ import React from 'react';
 import './SingleCrypto.scss';
 
 import LayoutFixedHeader from 'components/Layout/Layout-fixed-header';
-import { useEffect } from 'react/cjs/react.development';
+import { useEffect, useState } from 'react/cjs/react.development';
 import { useParams } from 'react-router-dom';
 
 
 export default function SingleCrypto() {
 
+    const [page, setPage] = useState({});
     const {id} = useParams();
 
     useEffect(() => {
         fetch(`${process.env.BACKEND}/crypto/${id}`)
         .then(res => res.json())
         .then(data => {
-            console.log(data);
+            setPage(data);
         })
         .catch(error => console.log(error));
-    })
+    }, []);
 
 
     return (
+        <>
+        <div style={{background: page.color}} className="bg-container">
+
+
+
+        </div>
         <LayoutFixedHeader>
-            <div className="bg-container">
-
-
-
-            </div>
+       
         </LayoutFixedHeader>
+        </>
     );
 }
