6b86cce3fd4e0fa05fe210606b1ff11a8b1fba84
Jason Ouyang <jasonouyang8000@gmail.com>
2021-06-26 20:25:33 -0700

    left side completed

--- src/components/RenderComp/RenderComp.js
+++ src/components/RenderComp/RenderComp.js
@@ -1,4 +1,4 @@
-import { faCoins, faDollarSign, faExchangeAlt, faLongArrowAltDown, faLongArrowAltUp } from '@fortawesome/free-solid-svg-icons';
+import { faCoins, faCrown, faDollarSign, faExchangeAlt, faHandPeace, faLongArrowAltDown, faLongArrowAltUp } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 const HtmlToReactParser = require('html-to-react').Parser;
@@ -7,15 +7,21 @@ export default function RenderCom({active,page}) {
     const htmlParser = new HtmlToReactParser();
     page.price = parseFloat(page.price).toFixed(6);
     page.btcPrice = parseFloat(page.btcPrice).toFixed(6);
-    page.increase = page.change > 0;
-    console.log(page);
+    page.increase = page.change > 0;    
+
+
+
+
+
     switch(active) {
         case 'Stats':
             return (
+                
+
                 <div className="stats-container ">
                     <div className="row">
                         <div className="col-12 col-lg-6 stats-box line-right">
-                            <h3>Price Stats</h3>
+                            <h3>Cool Stats</h3>
                             <div className="stats-column">
                                 <div className="col-4 d-flex align-items-center">
                                     <FontAwesomeIcon className="me-5" icon={faDollarSign} size='3x' />
@@ -42,6 +48,24 @@ export default function RenderCom({active,page}) {
                                 <div className="col-8 align-items-center d-flex justify-content-center">
                                     <p className={page.increase ? 'price-green' : 'price-red'}>{page.btcPrice}</p>
                                     <FontAwesomeIcon size='2x' className={page.increase ? 'price-green' : 'price-red'} icon={page.increase ? faLongArrowAltUp : faLongArrowAltDown} />
+                                </div>
+                            </div>
+                            <div className="stats-column">
+                                <div className="col-4   d-flex align-items-center">
+                                    <FontAwesomeIcon className="me-5" icon={faCrown} size='2x' />
+                                    <h2>Rank</h2>
+                                </div>
+                                <div className="col-8">
+                                    <p>{page.rank}</p>
+                                </div>
+                            </div>
+                            <div className="stats-column">
+                                <div className="col-4 d-flex align-items-center">
+                                    <FontAwesomeIcon className="me-5" icon={faHandPeace} size='2x' />
+                                    <h2>Best Price</h2>
+                                </div>
+                                <div className="col-8">
+                                    <p>{page.allTimeHigh ? parseFloat(page.allTimeHigh.price).toFixed(6) : 'null'}</p>
                                 </div>
                             </div>
 
