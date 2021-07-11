6c7fd6cb74ac280de317cf86751fb9d23ccdadc0
Jason Ouyang <jasonouyang8000@gmail.com>
2021-07-11 02:44:51 -0700

    more updates to search

--- src/components/SearchContent/SearchContent.js
+++ src/components/SearchContent/SearchContent.js
@@ -14,7 +14,6 @@ export default function SearchContent({searchInput, setSearchInput, closeModal})
         fetch(`${process.env.BACKEND}/crypto?search=${searchInput}`)
         .then(res => res.json())
         .then(data => {
-        
            setCoins(data.results.data.coins);
            setSearchInput('');
         })
@@ -25,7 +24,7 @@ export default function SearchContent({searchInput, setSearchInput, closeModal})
         <div className="row">
 
             {coins.map(coin => (
-                <div key={coin.uuid} className="col-4">
+                <div key={coin.uuid} className="col-md-4">
                     <Link onClick={closeModal} to={`/cryptos/${coin.uuid}`} className="search-card shadow">
                         <h3>{coin.symbol}</h3>
                         <img src={coin.iconUrl} alt={coin.name} />
